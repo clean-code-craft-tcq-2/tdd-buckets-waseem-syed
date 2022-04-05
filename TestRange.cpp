d9ecfdda2c5896b546af00b2d502254b891e7425
waseem-syed <91089691+waseem-syed@users.noreply.github.com>
2022-04-05 22:58:52 +0530

    Add files via upload

--- TestRange.cpp
+++ TestRange.cpp
@@ -3,42 +3,75 @@
 #include "Range.h"
 #include "A2DConverter.h"
 
-TEST_CASE("Test CalculateRangeAndReadings") 
+TEST_CASE("Test CalculateRangeAndReadings Continuous Reading present, Expect continuityInfo filled properly") 
 {
 
   std::vector<int> periodicCurrentInput {1, 2, 4};
   std::vector<ContinuityInfo> ContinuityInfoList;
   ContinuityInfoList = CalculateRangeAndReadings(periodicCurrentInput);
+  assert(1 == ContinuityInfoList.size());
   assert(true == ContinuityInfoList.size());
   REQUIRE(ContinuityInfoList[0].m_totalReadingContinuousRange == 2);
   REQUIRE(ContinuityInfoList[0].m_startIndexValueOfContinuousRange == 1);
   REQUIRE(ContinuityInfoList[0].m_endIndexValueOfContinuousRange == 2);
-	
+
   ::std::vector<int> periodicCurrentInput1 {3, 3, 5, 4, 10, 11, 12};
   std::vector<ContinuityInfo> ContinuityInfoList1;
   ContinuityInfoList1 = CalculateRangeAndReadings(periodicCurrentInput1);
- // assert(2 == ContinuityInfoList1.size());
+  assert(2 == ContinuityInfoList1.size());
   REQUIRE(ContinuityInfoList1[0].m_totalReadingContinuousRange == 4);
   REQUIRE(ContinuityInfoList1[0].m_startIndexValueOfContinuousRange == 3);
   REQUIRE(ContinuityInfoList1[0].m_endIndexValueOfContinuousRange == 5);
   REQUIRE(ContinuityInfoList1[1].m_totalReadingContinuousRange == 3);
   REQUIRE(ContinuityInfoList1[1].m_startIndexValueOfContinuousRange == 10);
   REQUIRE(ContinuityInfoList1[1].m_endIndexValueOfContinuousRange == 12);
-
 }
 
-TEST_CASE("Test checkForContinuityInPeriodicCurrent")
+TEST_CASE("Test CalculateRangeAndReadings No Continuous reading, Expect empty continuityInfo ")
 {
+  std::vector<int> periodicCurrentInput2 {1, 3, 5};
+  std::vector<ContinuityInfo> ContinuityInfoList2;
+  ContinuityInfoList2 = CalculateRangeAndReadings(periodicCurrentInput2);
+  assert(0 == ContinuityInfoList2.size());
+}
+
+
+TEST_CASE("Test checkForContinuityInPeriodicCurrent positive")
+{
+   std::vector<int> sortedPeriodicCurrents {1, 2, 4};
    std::vector<int> periodicCurrentInput {1, 2, 4};
    int CurrentIndexInPeriodicCurrentVector = 0;
    ContinuityInfo continuityInfo;
+   checkForContinuityInPeriodicCurrent(sortedPeriodicCurrents, CurrentIndexInPeriodicCurrentVector, continuityInfo);
    checkForContinuityInPeriodicCurrent(periodicCurrentInput, CurrentIndexInPeriodicCurrentVector, continuityInfo);
    REQUIRE(continuityInfo.m_totalReadingContinuousRange == 2);
    REQUIRE(continuityInfo.m_startIndexValueOfContinuousRange == 1);
    REQUIRE(continuityInfo.m_endIndexValueOfContinuousRange == 2);  
 }
 
-TEST_CASE("Test isValueContinuous")
+TEST_CASE("Test checkForContinuityInPeriodicCurrent Negative")
+{
+   CurrentIndexInPeriodicCurrentVector = 1;
+   ContinuityInfo continuityInfo1;
+   checkForContinuityInPeriodicCurrent(sortedPeriodicCurrents, CurrentIndexInPeriodicCurrentVector, continuityInfo1);
+   //default values
+   REQUIRE(continuityInfo1.m_totalReadingContinuousRange == 1);
+   REQUIRE(continuityInfo1.m_startIndexValueOfContinuousRange == 0);
+   REQUIRE(continuityInfo1.m_endIndexValueOfContinuousRange == 0); 
+}
+
+TEST_CASE("Test isValueContinuous Positive")
+{
+	currentIndexValue = 5;
+	followingIndexValue = 5;
+	REQUIRE(true == (isValueContinuous(currentIndexValue, followingIndexValue)));
+
+	currentIndexValue = 5;
+	followingIndexValue = 6;
+	REQUIRE(true == (isValueContinuous(currentIndexValue, followingIndexValue)));
+}
+
+TEST_CASE("Test isValueContinuous Negative")
 {
 	int currentIndexValue = 5;
 	int followingIndexValue = 7;
@@ -67,4 +100,7 @@ TEST_CASE("Test getAmpsInDigital")
 {
 	int ampsInAnalog = 4094;
 	REQUIRE((getAmpsInDigital(ampsInAnalog)) == 10);
+	
+	ampsInAnalog = 1146;
+	REQUIRE((getAmpsInDigital(ampsInAnalog)) == 3);
 }
